e6d49ca85638e1d0a5b5e3fea4039c05ec8bc9e3
Josias D. Rodriguez <yosiprompt@gmail.com>
2020-12-21 22:09:11 -0400

    carga script

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,7 +10,7 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    <script src="{{ asset('js/app.js') }}"  rel="javascript" type="text/javascript"></script>
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
