4dffe34161e1e297f08b4120b8399a416a81b82d
Josias D. Rodriguez <yosiprompt@gmail.com>
2020-12-23 04:06:11 -0400

    cambiar a ssl en production

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -25,6 +26,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        if(config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         Route::resourceVerbs([
             'create' => 'crear',
             'edit' => 'editar',
